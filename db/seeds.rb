25b9c072dfa8a58ac0fb1428f740a4ffef96aa9f
Juan Carlos Vergara <juanvergara9@hotmail.com>
2017-02-21 16:22:22 -0300

    tercer commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Category.destroy_all
+3.times do|t|
+	c=Category.create(nombre:"Categoria#{t}")
+	c.products.build(nombre:"producto#{t}", price: 2000)
+	Product.create(nombre:"producto#{t}", price: 3000, category_id: c.id)
+end
+
+Product.last.destroy
+p=Product.last
+p.premium = "TRUE"
+p.save
+
+Product.all.each do |x|
+	x.nombre = x.nombre.upcase
+	x.save
+end
